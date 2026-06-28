5bb83471bd8610a275f075004467cca89b7893db
cobra-r9 <cobra.rev.9@gmail.com>
2026-06-28 18:46:42 +0530

    added comments for strncpy, yet legacy

--- src/pomoc.c
+++ src/pomoc.c
@@ -19,9 +19,33 @@ static int send_command(const char *cmd, char *reply, int reply_size) {
     int fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (fd < 0) { perror("socket"); return -1; }
 
+
+    // let's now know the structure of the sockaddr_un what is it? 
+    // struct sockaddr_run {
+    //      sa_family_t sun_family;
+    //      char sun_path[108];
+    // }
+    // therefore, we need to first : zero the entire struct, avoid the garbage - done by memset. 
     struct sockaddr_un addr;
     memset(&addr, 0, sizeof(addr));
+
+    // set the sun family to AF_UNIX. 
     addr.sun_family = AF_UNIX;
+
+    // our socket : SOCKET_PATH is far more less bytes than the maximum size of the sun_path : 108 bytes. 
+    // accounting for the null terminator - it should be there always, for it to be a valid C string. 
+    // therefore, we can have a string path of length 107 and the last byte goes for the null terminator. 
+
+    // strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
+    // note that we have zeroed out the entire struct field via memset.
+    // we copy SOCKET_PATH to sun_path, that is, we copy, sizeof(addr.sun_path) - 1, that is 107 bytes from the SOCKET_PATH to the sun_path, case, the remaining are just nulled '\0', yet the path is valid. 
+
+
+    // the above strncpy has the following function signature : 
+    // strncpy : string do n copy (the expanded form, that is what I call it.)
+    // char *strncpy(char dst[restrict dsize], const char *restrict src, size_t dsize)
+    // here dsize is the destination size, src is the source string. 
+    // literally means - from the source string, copy dsize amount of bytes into the dst buffer (which by the signature's declaration is exactly dsize bytes in size. perfect match.)
     snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", SOCKET_PATH);
 
     if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
